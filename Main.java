8962ab7bb4705d8f8e534b702a6c7b2997e8af62
Shashwat Agarwal <54791282+s-hash-exe@users.noreply.github.com>
2020-12-12 21:35:48 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,11 +8,13 @@ public class Main
 	{
 		JFrame jf = new JFrame();
 		Gameplay gp = new Gameplay();
+		//Setting the screen size
 		jf.setBounds(10, 10, 700, 600);
 		jf.setTitle("Breakout Ball");
 		jf.setResizable(false);
 		jf.setVisible(true);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Adding a Panel in the Frame
 		jf.add(gp);
 	}
 }
